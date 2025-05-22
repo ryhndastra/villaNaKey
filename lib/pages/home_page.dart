e6cd879689487cb3ceeb4d782381017989b6e7eb
ryhndastra <ryhndastra@gmail.com>
2025-05-22 18:47:35 +0700

    rm: remove scaffold

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:villanakey/components/appbar_app.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-  void _goToLogin(BuildContext context) async {
-    final username = await Navigator.pushNamed(context, '/login');
 
-    if (username != null) {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text('Selamat datang, $username!')));
-      print('Login sukses user: $username');
-      // Update UI, simpan session, dll
-    }
+  void _goToLogin(BuildContext context) {
+    Navigator.pushNamed(context, '/login');
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarApp(onLoginPressed: () => _goToLogin(context)),
-      body: Center(child: Text("Home Page")),
+    return Center(
+      child: ElevatedButton(
+        onPressed: () => _goToLogin(context),
+        child: const Text("Home Page - Go to Login"),
+      ),
     );
   }
 }
