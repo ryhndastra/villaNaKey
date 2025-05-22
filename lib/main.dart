4ef76c2ff75e70fd50faf0a73000f9e7e2c308af
ryhndastra <ryhndastra@gmail.com>
2025-05-22 15:10:07 +0700

    feat : appbar + adding routes to login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:villanakey/components/splash_page.dart';
 import 'package:villanakey/pages/home_page.dart';
+import 'package:villanakey/pages/login_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,9 +14,11 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
       routes: {
         '/': (context) => const SplashPage(),
         '/home': (context) => const HomePage(),
+        '/login': (context) => const LoginPage(),
       },
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:villanakey/components/appbar_app.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  void _goToLogin(BuildContext context) async {
+    final username = await Navigator.pushNamed(context, '/login');
+
+    if (username != null) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Selamat datang, $username!')));
+      print('Login sukses user: $username');
+      // Update UI, simpan session, dll
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home Page")));
+    return Scaffold(
+      appBar: AppBarApp(onLoginPressed: () => _goToLogin(context)),
+      body: Center(child: Text("Home Page")),
+    );
   }
 }
